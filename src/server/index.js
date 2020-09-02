530391d88069070e977d049ab1601d785f2511c7
Hans Fonteyn <hans.fonteyn@infocura.be>
2020-09-02 17:17:02 +0200

    added cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = require('./router')
 const bodyparser = require('body-parser')
+var cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(bodyparser.json())
 app.use(router)
